91b88cdf8acd470b859d2c24a02907147902ba06
LeviathPro <leviathproductions@gmail.com>
2025-09-29 07:11:12 -0400

    Add animated gradient background utilities

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      keyframes: {
+        gradient: {
+          "0%, 100%": { "background-position": "0% 50%" },
+          "50%": { "background-position": "100% 50%" }
+        }
+      },
+      animation: {
+        gradient: "gradient 12s ease infinite"
+      }
+    }
+  },
   plugins: []
 };
